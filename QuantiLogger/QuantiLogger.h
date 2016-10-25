d0f8fb734d46a9807b6bcae64af9a51947a7a881
Martin Troup <troupmar@gmail.com>
2016-10-25 16:32:06 +0200

    Initial Commit

---
+++ QuantiLogger/QuantiLogger.h
@@ -0,0 +1,19 @@
+//
+//  QuantiLogger.h
+//  QuantiLogger
+//
+//  Created by Martin Troup on 25.10.16.
+//  Copyright © 2016 quanti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QuantiLogger.
+FOUNDATION_EXPORT double QuantiLoggerVersionNumber;
+
+//! Project version string for QuantiLogger.
+FOUNDATION_EXPORT const unsigned char QuantiLoggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QuantiLogger/PublicHeader.h>
+
+
