e1e934eea62f72f21452e9e3acb9066b6e5a76f6
Martin Troup <martin.troup@quanti.cz>
2017-09-05 14:40:38 +0200

    REF: TableViewDatasourceTest -> QuantiLoggerExample transition in progres…

---
+++ QuantiLoggerExample/LogListTableViewController.swift
@@ -0,0 +1,69 @@
+//
+//  LogListTableViewController.swift
+//  2N-mobile-communicator
+//
+//  Created by Martin Troup on 09.11.16.
+//  Copyright © 2016 quanti. All rights reserved.
+//
+
+import UIKit
+
+class LogListTableViewController: UIViewController {
+    private let refreshControl = UIRefreshControl()
+
+    let logListTableView = UITableView()
+
+    let fileLoggerTableViewDatasource = FileLoggerTableViewDatasource()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+
+        view.addSubview(logListTableView)
+
+        logListTableView.translatesAutoresizingMaskIntoConstraints = false
+
+        logListTableView.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor).isActive = true
+        logListTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        logListTableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        logListTableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+
+        logListTableView.delegate = self
+        logListTableView.dataSource = fileLoggerTableViewDatasource
+        logListTableView.register(FileLoggerTableViewCell.self, forCellReuseIdentifier: QuantiLoggerConstants.FileLoggerTableViewDatasource.fileLoggerTableViewCellIdentifier)
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+
+        }
+
+
+        // Pull to refresh
+        refreshControl.addTarget(self, action: #selector(didPullToRefresh), for: .valueChanged)
+        logListTableView.addSubview(refreshControl)
+    }
+
+    dynamic private func didPullToRefresh(_ sender: UIRefreshControl) {
+        fileLoggerTableViewDatasource.reload()
+        logListTableView.reloadData()
+
+        refreshControl.endRefreshing()
+    }
+}
+
+
+extension LogListTableViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+}
+
+
---
+++ QuantiLoggerExample/SendLogFilesViaMailViewController.swift
@@ -0,0 +1,42 @@
+//
+//  SendLogFilesViaMailViewController.swift
+//  QuantiLogger
+//
+//  Created by Martin Troup on 10.11.16.
+//  Copyright © 2016 quanti. All rights reserved.
+//
+
+import UIKit
+import MessageUI
+
+class SendLogFilesViaMailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+
+
+        if MFMailComposeViewController.canSendMail() {
+            let logFilesViaMailViewController = LogFilesViaMailViewController(withRecipients: ["mbigmac@seznam.cz"])
+            logFilesViaMailViewController.mailComposeDelegate = self
+
+            present(logFilesViaMailViewController, animated: true, completion: nil)
+        } else {
+            let sendMailErrorAlert = UIAlertController(title: "Could Not Send Email",
+                                                       message: "Your device could not send e-mail.  Please check e-mail configuration and try again.",
+                                                       preferredStyle: .alert)
+            sendMailErrorAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (_) in
+                sendMailErrorAlert.dismiss(animated: true, completion: nil)
+            }))
+
+            present(sendMailErrorAlert, animated: true, completion: nil)
+        }
+    }
+}
+
+extension SendLogFilesViaMailViewController: MFMailComposeViewControllerDelegate {
+    public func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+}
