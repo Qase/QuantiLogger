9be814c7fa9afcc429233e0b35c314bb671ae955
georgeivannikov <41365858+georgeivannikov@users.noreply.github.com>
2019-12-10 13:52:11 +0100

    WIP: Draft version for test

--- QuantiLogger/common/FileLoggerManager.swift
+++ QuantiLogger/common/FileLoggerManager.swift
@@ -133,7 +133,7 @@ class FileLoggerManager {
         }
 
         print("Size1:")
-        print(fileSize(forURL: archive.url))
+//        print(fileSize(forURL: archive.url))
         
         
         do {
@@ -162,7 +162,7 @@ class FileLoggerManager {
 //        }
         
         print("Size2:")
-        print(fileSize(forURL: archive.url))
+//        print(fileSize(forURL: archive.url))
         
         do {
             let resources = try archive.url.resourceValues(forKeys:[.fileSizeKey])
@@ -383,22 +383,3 @@ class FileLoggerManager {
     }
 }
 
-func fileSize(forURL url: Any) -> Double {
-    var fileURL: URL?
-    var fileSize: Double = 0.0
-    if (url is URL) || (url is String)
-    {
-        if (url is URL) {
-            fileURL = url as? URL
-        }
-        else {
-            fileURL = URL(fileURLWithPath: url as! String)
-        }
-        var fileSizeValue = 0.0
-        try? fileSizeValue = (fileURL?.resourceValues(forKeys: [URLResourceKey.fileSizeKey]).allValues.first?.value as! Double?)!
-        if fileSizeValue > 0.0 {
-            fileSize = (Double(fileSizeValue) / (1024 * 1024))
-        }
-    }
-    return fileSize
-}
