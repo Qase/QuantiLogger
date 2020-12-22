8d9dede60aa2a5aca03b70f9bd4c3e20ea477af3
VS <shmatvic@gmail.com>
2020-12-22 13:52:15 +0100

    FIX: RxSwift up to next major starting with 5

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "QuantiLogger",
-            targets: ["QuantiLogger", "QuantiLoggerExample"]),
+            targets: ["QuantiLogger"]),
     ],
     dependencies: [
         .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.0.0"))
@@ -23,15 +23,14 @@ let package = Package(
         .target(name: "QuantiLogger",
                 dependencies: [.product(name: "RxCocoa", package: "RxSwift"),
                                .product(name: "RxSwift", package: "RxSwift")],
-                path: "QuantiLogger",
-                exclude: ["ios/Info.plist", "mac/InfoMac.plist"]
+                path: "QuantiLogger"
         ),
-        .target(name: "QuantiLoggerExample",
-                dependencies: [.product(name: "RxCocoa", package: "RxSwift"),
-                               .product(name: "RxSwift", package: "RxSwift"),
-                               .target(name: "QuantiLogger")],
-                path: "QuantiLoggerExample"
-        ),
+//        .target(name: "QuantiLoggerExample",
+//                dependencies: [.product(name: "RxCocoa", package: "RxSwift"),
+//                               .product(name: "RxSwift", package: "RxSwift"),
+//                               .target(name: "QuantiLogger")],
+//                path: "QuantiLoggerExample"
+//        ),
         .testTarget(name: "QuantiLoggerTests",
                     dependencies: [.target(name: "QuantiLogger")],
                     path: "QuantiLoggerTests")
