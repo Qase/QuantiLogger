26a4aa68c0f4e231b8b6085993bd4d481364836c
David Nemec <david2xn@gmail.com>
2020-12-30 12:04:02 +0100

    FEAT: Compilable for mac target

--- Package.swift
+++ Package.swift
@@ -12,10 +12,10 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "QuantiLogger",
-            targets: ["QuantiLogger", "QuantiLoggerExample"]),
+            targets: ["QuantiLogger"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", .exact("6.0.0-rc.2"))
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.1.1"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
--- QuantiLogger/mac/QuantiLoggerMac.h
+++ QuantiLogger/mac/QuantiLoggerMac.h
@@ -1,6 +1,6 @@
 //
-//  QuantiLoggerMac.h
-//  QuantiLoggerMac
+//  QuantiLogger.h
+//  QuantiLogger
 //
 //  Created by Dagy Tran on 24/06/2019.
 //  Copyright © 2019 quanti. All rights reserved.
@@ -9,11 +9,11 @@
 #import <Cocoa/Cocoa.h>
 
 //! Project version number for QantiLoggerMac.
-FOUNDATION_EXPORT double QuantiLoggerMacVersionNumber;
+FOUNDATION_EXPORT double QuantiLoggerVersionNumber;
 
 //! Project version string for QantiLoggerMac.
-FOUNDATION_EXPORT const unsigned char QuantiLoggerMacVersionString[];
+FOUNDATION_EXPORT const unsigned char QuantiLoggerVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <QuantiLoggerMac/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like #import <QuantiLogger/PublicHeader.h>
 
 
