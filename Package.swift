c8e6c32b18eb240f20d299039dc03b70e30f7131
David Nemec <david.nemec@quanti.cz>
2023-02-27 18:13:38 +0100

    FEAT: Remove RxSwift dependency (and remove weblogger)

--- Package.swift
+++ Package.swift
@@ -15,14 +15,13 @@ let package = Package(
             targets: ["QuantiLoggerMac"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "4.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "QuantiLoggerMac",
-            dependencies: ["RxSwift", "RxCocoa"],
+            dependencies: [],
             path: "QuantiLogger/common"
         )
     ]
